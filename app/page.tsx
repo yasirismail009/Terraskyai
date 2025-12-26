f0f81737438d9ec1230a95d01ce28c7f0e6961b3
Muhammad Yasir Ismail <yasir@lmntrix.com>
2025-12-26 17:07:36 +0500

    initial push

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
         id="home"
         className="relative min-h-[90vh] flex items-center justify-center px-4 sm:px-6 lg:px-8 py-8 font-sans bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `url('/assets/img_0486.jpeg')`,
+          backgroundImage: `url('/assets/main bg.png')`,
         }}
       >
         {/* Overlay for text readability */}
