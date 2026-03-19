8c7fb164ad9bd02185687353fa19e0077d7adc2b
manshoor-ali <syed@i-labs.app>
2026-03-19 17:18:53 +0500

    test

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -130,7 +130,7 @@ export default function Footer() {
                 Privacy Policy
               </a>
               <a href="#" className="hover:text-[#545454] transition-colors">
-                Terms of Servicex
+                Terms of Service
               </a>
             </div>
           </div>
